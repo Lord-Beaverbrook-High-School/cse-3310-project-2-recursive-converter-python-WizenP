c0e538cfe51f84deb6b463964b7173b3554064c2
Wizen <gagep10@educbe.ca>
2025-03-12 10:42:33 -0600

    commented and functional

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def decToHex(dec):
 
 
 def hexToDec(hex):
+    # makes sure alphabetical characters are in the right case
+    hex = hex.lower()
     # dict to store the equivalents of each hex value
     hex_values = {
         "0": 0,
@@ -23,18 +25,20 @@ def hexToDec(hex):
         "7": 7,
         "8": 8,
         "9": 9,
-        "A": 10,
-        "B": 11,
-        "C": 12,
-        "D": 13,
-        "E": 14,
-        "F": 15
+        "a": 10,
+        "b": 11,
+        "c": 12,
+        "d": 13,
+        "e": 14,
+        "f": 15
     }
     # exponent to raise 16 to
     expo = len(hex) - 1
-    #
+    # stops the recursion if no further operations are necessary
     if expo == 0:
         return hex_values[hex]
+    # recursive algorithm that adds the first value multiplied by the 16 to the power of the necessary exponent
+    # to the output of the function, iterating through each element and giving the final value in decimal
     else:
         return hexToDec(hex[1:]) + (hex_values[hex[0]]) * (16 ** expo)
 
@@ -45,22 +49,27 @@ def binToDec(bin):
         return 0
     if bin == "1":
         return 1
-    # 
+    # "binToDec(bin[:-1])" recursively loops back through the function, removing the last digit of the input
+    # "binToDec(bin[-1])" loops the final value back to the start of the function, returning either a 1 or 0
     return binToDec(bin[:-1]) * 2 + binToDec(bin[-1])
 
 
 def decToBin(dec):
-    end_string = ""
-    if dec > 1:
-        decToBin(dec // 2)
-    end_string += str(dec % 2)
+    # end point for recursion
+    if dec == 1:
+        return dec
+    # recursive loop that divides the current value by 2, and truncates, then adds the remainder of that to the output
+    else:
+        return str(decToBin(dec // 2)) + str(dec % 2)
 
 
+# value inputs
 dec_value = int(input("Please input a decimal value: "))
-hex_value = input("Please input a hexadecimal value: ")
+hex_value = str(input("Please input a hexadecimal value: "))
 bin_value = input("Please input a binary value: ")
 
-print(decToHex(dec_value))
-print(hexToDec(hex_value))
-print(decToBin(dec_value))
-print(binToDec(bin_value))
+# print statements
+print("Decimal to Hexadecimal:", decToHex(dec_value))
+print("Hexadecimal to Decimal:", hexToDec(hex_value))
+print("Decimal to Binary:", decToBin(dec_value))
+print("Binary to Decimal:", binToDec(bin_value))
